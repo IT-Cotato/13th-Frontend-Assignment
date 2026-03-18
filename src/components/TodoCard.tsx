a4d2ab8ccbd809e4401d929f4aeae942a46e50c2
Eunjin-n <ikee1652@duksung.ac.kr>
2026-03-18 14:20:17 +0900

    week1 과제 제출

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,45 @@
+import { ToDoList } from './TodoList';
+
+export function Card1() {
+  return (
+    <>
+      <div className="card">
+        <p>{ToDoList.fi}</p>
+      </div>
+  
+    </>
+  );
+}
+
+export function Card2() {
+  return (
+    <>
+      <div className="card">
+        <p>{ToDoList.se}</p>
+      </div>
+  
+    </>
+  );
+}
+
+export function Card3() {
+  return (
+    <>
+      <div className="card">
+        <p>{ToDoList.th}</p>
+      </div>
+  
+    </>
+  );
+}
+
+export function Card4() {
+  return (
+    <>
+      <div className="card">
+        <p>{ToDoList.fo}</p>
+      </div>
+  
+    </>
+  );
+}
