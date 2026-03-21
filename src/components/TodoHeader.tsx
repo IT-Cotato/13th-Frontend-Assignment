32b273d2b950f973fdf4142423af507d648c7a69
hyotju <joseph20010604@gmail.com>
2026-03-21 22:50:17 +0900

    과제 제출합니다

---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,9 @@
+export default function TodoHeader(){
+    return (
+        <header className="todo-header">
+            <p className="subtitle"> Week 1 ㅡ 기본 레이아웃</p>
+            <h1 className="title">✅ 오늘의 할 일</h1>
+        </header>
+    );
+
+}
