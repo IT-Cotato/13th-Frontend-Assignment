da3c1631fa8e29ca40858ee62f49cd7a362a0c78
metjyc <metjyc@naver.com>
2026-03-23 11:09:04 +0900

    refactor: todoheader 구조 개편

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -5,19 +5,12 @@ type TodoHeaderProps = {
 
 function TodoHeader({ icon, title }: TodoHeaderProps) {
   return (
-    <div className="self-stretch py-2.5 flex flex-col justify-start items-start gap-2.5">
-      <div className="self-stretch h-9 inline-flex justify-start items-center gap-2">
-        <div className="w-8 h-9 flex items-center justify-start">
-          <div className="text-gray-800 text-2xl font-bold leading-9">
-            {icon}
-          </div>
-        </div>
-
-        <div className="h-9 flex items-center justify-start">
-          <div className="text-gray-800 text-2xl font-bold leading-9">
-            {title}
-          </div>
-        </div>
+    <div className="self-stretch py-2.5">
+      <div className="flex h-9 items-center gap-2">
+        <span className="text-gray-800 text-2xl font-bold leading-9">
+          {icon}
+        </span>
+        <h1 className="text-gray-800 text-2xl font-bold leading-9">{title}</h1>
       </div>
     </div>
   );
