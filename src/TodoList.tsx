e744990f708fffa2193174c87162f0c1fca59492
YoungI422 <dlsrudtj@gmail.com>
2026-03-26 02:05:09 +0900

    refactor: 할 일 목록 구조를 ul, li 시맨틱 태그로 변경

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -3,11 +3,11 @@ import TodoCard from "./TodoCard";
 export default function TodoList() {
     
   return (
-    <div className="frame1">
-      <TodoCard text="리액트 공식문서 읽기" />
-      <TodoCard text="알고리즘 문제 풀기" />
-      <TodoCard text="운동 30분 하기" />
-      <TodoCard text="프로젝트 회의 준비" />
-    </div>
+    <ul className="frame1">
+      <li><TodoCard text="리액트 공식문서 읽기" /></li>
+      <li><TodoCard text="알고리즘 문제 풀기" /></li>
+      <li><TodoCard text="운동 30분 하기" /></li>
+      <li><TodoCard text="프로젝트 회의 준비" /></li>
+    </ul>
   )
 }
