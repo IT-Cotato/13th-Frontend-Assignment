75c63ec2cbb5df411895d38ecb901dd25468718d
Kim-Kiseong <kiseong054241@gmail.com>
2026-03-25 23:38:32 +0900

    Ready to week 2

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,7 +1,11 @@
 // src/components/TodoCard.tsx
 import React from 'react';
 
+<<<<<<< Updated upstream
 // props 타입을 정의합니다 (TypeScript용)
+=======
+// props 타입을 정의
+>>>>>>> Stashed changes
 interface TodoCardProps {
   content: string;
 }
--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -2,7 +2,11 @@
 import React from 'react';
 
 const TodoHeader = () => {
+<<<<<<< Updated upstream
   const mainTitle = "✅ 오늘의 할 일"; // JSX 바인딩을 위한 변수
+=======
+  const mainTitle = "✅ 오늘의 할 일"; // JSX 바인딩 변수
+>>>>>>> Stashed changes
 
   return (
     <header className="todo-header">
