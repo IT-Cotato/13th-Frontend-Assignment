2f878a748f353727f01a0ca0be9fa02ceed20f01
임유미 <dladbal24@sookmyaung.ac.kr>
2026-03-27 06:17:35 +0900

    refactor&style: 리스트 태그 변경/리스트 스타일 수정

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,9 +1,26 @@
 import TodoCard from "./TodoCard";
 
 export default function TodoList() {
+    /*
+    const todoItems = [
+        { text: "리액트 공식문서 읽기" },
+        { text: "알고리즘 문제 풀기" },
+        { text: "운동 30분 하기" },
+        { text: "프로젝트 회의 준비" },
+    ];
+*/
+    /* 리스트 목록을 돌며 여러 카드를 렌더링하도록 수정 */
     return (
-        <div id="todoList">
-            <TodoCard />
-        </div>
+        <ul id="todoList">
+            <li className="todoCard">
+                <TodoCard text="리액트 공식문서 읽기" />
+            </li>
+            <li className="todoCard">
+                <TodoCard text="리액트 공식문서 읽기" />
+            </li>
+            <li className="todoCard">
+                <TodoCard text="리액트 공식문서 읽기" />
+            </li>
+        </ul>
     );
 }
