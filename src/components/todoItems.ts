e7985fcff10515325fafe364f70dc8644ebc12c4
Eunjin-n <ikee1652@duksung.ac.kr>
2026-03-26 15:32:16 +0900

    feat: 데이터 변경 및 파일 확장자 변경

---
+++ src/components/todoItems.ts
@@ -0,0 +1,22 @@
+export const TODO_ITEMS = [ 
+  {
+    id: 0,
+    name: "리액트 공식문서 읽기",
+    isChecked: true,
+  },
+  {
+    id: 1,
+    name: "알고리즘 문제 풀기",
+    isChecked: true,
+  },
+  {
+    id: 2,
+    name: "운동 30분 하기",
+    isChecked: false,
+  },
+  {
+    id: 3,
+    name: "프로젝트 회의 준비",
+    isChecked: false,
+  }
+];
