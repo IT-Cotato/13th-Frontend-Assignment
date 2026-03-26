859b454c37e2cfa9232b25a7fb110387f014eac1
임유미 <dladbal24@sookmyaung.ac.kr>
2026-03-27 08:39:31 +0900

    style: 완료된 할 일 스타일 추가

--- src/TodoCard.tsx
+++ src/TodoCard.tsx
@@ -1,3 +1,25 @@
-export default function TodoCard({ text }: { text: string }) {
-    return <>{text}</>;
+import { FaCheck } from "react-icons/fa6";
+
+export default function TodoCard({
+    id,
+    text,
+    isChecked,
+}: {
+    id: string;
+    text: string;
+    isChecked: boolean;
+}) {
+    return (
+        <>
+            <li className={"todoCard " + (isChecked ? "checked" : "")}>
+                <input type="checkbox" checked={isChecked} id={id} />
+                <label htmlFor={id}>
+                    <span className="check-icon">
+                        {isChecked && <FaCheck color="white" size={14} />}
+                    </span>
+                    {isChecked ? <del>{text}</del> : text}
+                </label>
+            </li>
+        </>
+    );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -11,16 +11,11 @@ export default function TodoList() {
 */
     /* 리스트 목록을 돌며 여러 카드를 렌더링하도록 수정 */
     return (
-        <ul id="todoList">
-            <li className="todoCard">
-                <TodoCard text="리액트 공식문서 읽기" />
-            </li>
-            <li className="todoCard">
-                <TodoCard text="리액트 공식문서 읽기" />
-            </li>
-            <li className="todoCard">
-                <TodoCard text="리액트 공식문서 읽기" />
-            </li>
+        <ul id="todoList" className="control-container">
+            <TodoCard id="1" text="리액트 공식문서 읽기" isChecked={true} />
+            <TodoCard id="2" text="알고리즘 문제 풀기" isChecked={true} />
+            <TodoCard id="3" text="운동 30분 하기" isChecked={false} />
+            <TodoCard id="4" text="프로젝트 회의 준비" isChecked={false} />
         </ul>
     );
 }
