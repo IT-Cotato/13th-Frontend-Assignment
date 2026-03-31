a02a42ff076b4b69d748b091cd72a297c6abfe55
metjyc <metjyc@naver.com>
2026-03-31 12:51:43 +0900

    refactor: 수정사항 반영

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -5,20 +5,9 @@ type TodoHeaderProps = {
 
 function TodoHeader({ icon, title }: TodoHeaderProps) {
   return (
-    <div className="self-stretch py-2.5 flex flex-col justify-start items-start gap-2.5">
-      <div className="self-stretch h-9 inline-flex justify-start items-center gap-2">
-        <div className="w-8 h-9 flex items-center justify-start">
-          <div className="text-gray-800 text-2xl font-bold leading-9">
-            {icon}
-          </div>
-        </div>
-
-        <div className="h-9 flex items-center justify-start">
-          <div className="text-gray-800 text-2xl font-bold leading-9">
-            {title}
-          </div>
-        </div>
-      </div>
+    <div className="flex self-stretch items-center gap-2 py-2.5">
+      <span className="text-gray-800 text-2xl font-bold leading-9">{icon}</span>
+      <h1 className="text-gray-800 text-2xl font-bold leading-9">{title}</h1>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,21 @@
 import TodoCard from "./TodoCard";
 
 type TodoListProps = {
-  todo1: string;
-  todo2: string;
-  todo3: string;
-  todo4: string;
+  todos: string[];
 };
 
-function TodoList({ todo1, todo2, todo3, todo4 }: TodoListProps) {
+function TodoList({ todos }: TodoListProps) {
   return (
-    <div className="self-stretch h-64 flex flex-col justify-start items-start gap-4">
-      <div className="self-stretch flex-1 flex flex-col justify-start items-start gap-4">
-        <TodoCard text={todo1} />
-        <TodoCard text={todo2} />
-        <TodoCard text={todo3} />
-        <TodoCard text={todo4} />
-      </div>
-    </div>
+    <ul className="self-stretch h-64 list-none m-0 flex flex-col justify-start items-start gap-4 p-0">
+      {todos.map((todo, index) => (
+        <li
+          key={index}
+          className="self-stretch flex-1 flex flex-col justify-start items-start"
+        >
+          <TodoCard text={todo} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
