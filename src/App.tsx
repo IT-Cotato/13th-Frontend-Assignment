f747c5760f05b61df85576eb30bdc8be06ef0124
sumin <sumin990103@naver.com>
2026-04-01 01:48:33 +0900

    feat: todo 입력 폼 UI 구현 및 상태 관리 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import TodoHeader from './components/TodoHeader'
 import TodoList from './components/TodoList'
+import TodoForm from './components/TodoInput'
 
 function App() {
   return (
     <div className="flex min-h-screen w-full items-start bg-[#F5F5F5]">
       <div className="mx-auto flex w-full max-w-[640px] flex-col items-start gap-[22px]">
         <TodoHeader />
+        <TodoForm />
         <TodoList />
       </div>
     </div>
