699a54e1be526f502f53b9e3c7ea82df3eea4da0
seo1v <minseoeum14@gmail.com>
2026-04-02 00:40:30 +0900

    feat: 입력 중 상태 구현

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,12 +1,24 @@
 import "./TodoInput.css"
+import { useState } from "react";
 
 function TodoInput() {
+  const [text, setText] = useState("");
+
+  const handleFocus = () => {
+    if (text === "") {
+      setText("새로운 할 일");
+    }
+  };
+
   return (
     <div className="input-section">
       <input 
         type="text" 
         placeholder="할 일을 입력하세요" 
         className="todo-input" 
+        value={text}
+        onFocus={handleFocus}
+        onChange={(e) => setText(e.target.value)}
       />
       <button className="add-button">추가</button>
     </div>
