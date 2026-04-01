1dd15d15ed10e69507d1e6249ee7b08f6f43d8b2
YoungI422 <dlsrudtj@gmail.com>
2026-04-01 21:42:22 +0900

    style: 체크박스 border-radius 수정 및 SVG 속성 수정

--- src/CheckIcon.tsx
+++ src/CheckIcon.tsx
@@ -1,7 +1,19 @@
 export default function CheckIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="10" viewBox="0 0 14 10" fill="none">
-      <path d="M1 5L5 9L13 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="10"
+      viewBox="0 0 14 10"
+      fill="none"
+    >
+      <path
+        d="M1 5L5 9L13 1"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 }
