0fd53a42093d9ad6d5235b4b805431154f896542
sxxbxxny <gyg031@naver.com>
2026-04-02 19:36:49 +0900

    허수빈 week3 과제 수정

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -27,42 +27,40 @@ export default function TodoList({
   onFocus,
   onBlur,
 }: TodoListProps) {
-  if (items.length === 0) {
-    return (
-      <div className="todo-empty">
-        <span className="todo-empty__icon">{"📋"}</span>
-        <p className="todo-empty__text">{"아직 할 일이 없어요"}</p>
+  return (
+    <div className="todo-list">
+      <div className="todo-input-card">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="새로운 할 일"
+          value={inputValue}
+          onChange={(e) => onInputChange(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && onAdd()}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+        <button className="add-button" onClick={onAdd}>
+          추가
+        </button>
       </div>
-    );
-  }
 
-return (
-  <div className="todo-list">
-    <div className="todo-input-card">
-      <input
-        type="text"
-        className="todo-input"
-        placeholder="새로운 할 일"
-        value={inputValue}
-        onChange={(e) => onInputChange(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && onAdd()}
-        onFocus={onFocus}
-        onBlur={onBlur}
-      />
-      <button className="add-button" onClick={onAdd}>
-        추가
-      </button>
+      {items.length === 0 ? (
+        <div className="todo-empty">
+          <span className="todo-empty__icon">{"📋"}</span>
+          <p className="todo-empty__text">{"아직 할 일이 없어요"}</p>
+        </div>
+      ) : (
+        items.map((item) => (
+          <TodoCard
+            key={item.id}
+            text={item.text}
+            done={item.done}
+            onDelete={() => onDelete(item.id)}
+            onToggle={() => onToggle(item.id)}
+          />
+        ))
+      )}
     </div>
-
-    {items.map((item) => (
-      <TodoCard
-        key={item.id}
-        text={item.text}
-        done={item.done}
-        onDelete={() => onDelete(item.id)}
-        onToggle={() => onToggle(item.id)}
-      />
-    ))}
-  </div>
-);
+  );
 }
