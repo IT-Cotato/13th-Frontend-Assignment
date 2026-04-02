75e83da043873d18882f23d8ebf90f58b8845039
YoungI422 <dlsrudtj@gmail.com>
2026-04-02 22:42:41 +0900

    feat: useState를 이용한 초기 할 일 목록과 입력창 상태 선언

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import TodoHeader from "./TodoHeader"
-import TodoList from "./TodoList"
-import './App.css'
+import { useState } from "react";
+import TodoHeader from "./TodoHeader";
+import TodoList from "./TodoList";
+import "./App.css";
 
-const todos = [
+const fixedTodos = [
   { id: 1, text: "리액트 공식문서 읽기", isCompleted: true },
   { id: 2, text: "알고리즘 문제 풀기", isCompleted: true },
   { id: 3, text: "운동 30분 하기", isCompleted: false },
-  { id: 4, text: "프로젝트 회의 준비", isCompleted: false }, 
+  { id: 4, text: "프로젝트 회의 준비", isCompleted: false },
+  { id: 5, text: "장보기 하기", isCompleted: false },
 ];
 
 function App() {
-  
+  const [todos, setTodos] = useState(fixedTodos);
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="frame3">
       <div className="frame2">
@@ -20,7 +24,7 @@ function App() {
         <TodoList todos={todos} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
