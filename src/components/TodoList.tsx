bbdeb116737fd74400123ffd47b8241330f0ffe1
sxxbxxny <gyg031@naver.com>
2026-04-02 19:01:53 +0900

    week3 허수빈 과제

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,9 +8,25 @@ type Todo = {
 
 type TodoListProps = {
   items: Todo[];
+  inputValue: string;
+  onInputChange: (value: string) => void;
+  onAdd: () => void;
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
+  onFocus: () => void;
+  onBlur: () => void;
 };
 
-export default function TodoList({ items }: TodoListProps) {
+export default function TodoList({
+  items,
+  inputValue,
+  onInputChange,
+  onAdd,
+  onDelete,
+  onToggle,
+  onFocus,
+  onBlur,
+}: TodoListProps) {
   if (items.length === 0) {
     return (
       <div className="todo-empty">
@@ -20,11 +36,33 @@ export default function TodoList({ items }: TodoListProps) {
     );
   }
 
-  return (
-    <div className="todo-list">
-      {items.map((item) => (
-        <TodoCard key={item.id} text={item.text} done={item.done} />
-      ))}
+return (
+  <div className="todo-list">
+    <div className="todo-input-card">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="새로운 할 일"
+        value={inputValue}
+        onChange={(e) => onInputChange(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && onAdd()}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      <button className="add-button" onClick={onAdd}>
+        추가
+      </button>
     </div>
-  );
+
+    {items.map((item) => (
+      <TodoCard
+        key={item.id}
+        text={item.text}
+        done={item.done}
+        onDelete={() => onDelete(item.id)}
+        onToggle={() => onToggle(item.id)}
+      />
+    ))}
+  </div>
+);
 }
