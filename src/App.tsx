1a5ed37f8f5f355014705b1c05e11284a8a6dbed
kdhye516 <kdhye2005@gmail.com>
2026-04-02 18:59:01 +0900

    김다혜 week3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import "./App.css";
 import TodoHeader from "./TodoHeader";
 import TodoList from "./TodoList";
+import { todos } from "./todos.data";
 
 function App() {
-  const todos = [
-    { id: 1, text: "리액트 공식문서 읽기", isDone: true },
-    { id: 2, text: "알고리즘 문제 풀기", isDone: true },
-    { id: 3, text: "운동 30분 하기", isDone: false },
-    { id: 4, text: "프로젝트 회의 준비", isDone: false },
-  ];
 
   return (
     <div className="frame3">
---
+++ src/todos.data.ts
@@ -0,0 +1,6 @@
+export const todos = [
+  { id: 1, text: "리액트 공식문서 읽기", isDone: true },
+  { id: 2, text: "알고리즘 문제 풀기", isDone: true },
+  { id: 3, text: "운동 30분 하기", isDone: false },
+  { id: 4, text: "프로젝트 회의 준비", isDone: false },
+];
