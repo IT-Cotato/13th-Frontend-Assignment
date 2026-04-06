ecdc4798fc8af59414181520cc9fb80b6244eb09
임유미 <dladbal24@sookmyung.ac.kr>
2026-04-06 12:26:49 +0900

    style: 삭제 버튼, 추가 버튼 스타일 수정

--- src/DeleteButton.tsx
+++ src/DeleteButton.tsx
@@ -1,7 +1,11 @@
+import trashIcon from "./resource/trashcan-icon.png";
+
 export default function DeleteButton({ onDelete }) {
     return (
         <>
-            <button onClick={() => onDelete()}>🗑</button>
+            <button id="delete-button" onClick={() => onDelete()}>
+                <img src={trashIcon} alt="삭제" width={20} height={20} />
+            </button>
         </>
     );
 }
