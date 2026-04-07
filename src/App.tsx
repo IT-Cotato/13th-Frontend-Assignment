9ecdb0b0acbaa927d317b0940e23ef57ed22b41b
sumin <sumin990103@naver.com>
2026-04-08 03:19:32 +0900

    feat: 할 일 카운터 기능 구현(TodoOverview)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import TodoHeader from './components/TodoHeader'
 import TodoList from './components/TodoList'
 import TodoInput from './components/TodoInput'
+import TodoOverview from './components/TodoOverview'
 import {todoData} from './data/TodoData'
 import {useState} from 'react';
 
@@ -37,6 +38,7 @@ function App() {
     <div className="flex min-h-screen w-full items-start bg-[#F5F5F5]">
       <div className="mx-auto flex w-full max-w-[640px] flex-col items-start gap-[22px]">
         <TodoHeader />
+        <TodoOverview todos={todos} />
         <TodoInput onAddTodo={handleAddTodo} />
         <TodoList todos={todos} onToggle={handleToggle} onDelete={handleDelete} />
       </div>
