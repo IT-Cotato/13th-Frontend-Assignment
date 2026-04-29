8ad362faa799a86981e8cb2c83d3e91a3922d712
qkrekdls <gunmannduu@gmail.com>
2026-04-29 18:21:59 +0900

    feat: WEEK5

--- src/component/TodoCard.tsx
+++ src/component/TodoCard.tsx
@@ -1,13 +1,31 @@
 import checkIcon from "../assets/Check.svg";
-
+import { CATEGORY_STYLES, type CategoryType } from "../constants/category";
+import { useState } from "react";
 interface TodoCardProps {
   content: string;
-  checked?: boolean;
-  onToggle?: () => void;
-  onDelete?: () => void;
+  checked: boolean;
+  category: CategoryType;
+  onToggle: () => void;
+  onDelete: () => void;
+  onUpdate: (newContent: string) => void;
 }
 
-export default function TodoCard({ content, checked, onToggle, onDelete }: TodoCardProps) {
+export default function TodoCard({ content, checked, category, onToggle, onDelete, onUpdate }: TodoCardProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(content);
+
+  const handleSave = () => {
+    if (editValue.trim().length === 0) return;
+    onUpdate(editValue);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setEditValue(content);
+    setIsEditing(false);
+  };
+
+  const style = CATEGORY_STYLES[category]
   return (
     <div className="flex p-4 items-center justify-between self-stretch bg-card rounded-[12px] shadow-[0_1px_3px_0_rgba(0,0,0,0.10)]">
 
@@ -26,20 +44,64 @@ export default function TodoCard({ content, checked, onToggle, onDelete }: TodoC
           )}
         </div>
 
-        <p className={`text-body leading-[21px] transition-all ${
-          checked ? 'line-through text-gray-400 opacity-60' : 'text-gray-900'
-        }`}
-        >
-          {content}
-        </p>
+        <div className="flex flex-col w-[442px] gap-2 items-start justify-center">
+          {isEditing ? (
+            <input 
+              value={editValue}
+              onChange={(e) => setEditValue(e.target.value)}
+              className="w-full h-[41px] px-2 py-3 items-start rounded-[8px] border-2 border-primary text-body text-text bg-transparent ring-4 ring-primary"
+            />
+          ) : (
+            <p className={`text-body leading-[21px] transition-all ${
+              checked ? 'line-through text-gray-400 opacity-60' : 'text-gray-900'
+            }`}
+            >
+              {content}
+            </p>
+          )}
+          <span className={`px-3 py-1 rounded-full text-caption ${style.tag}`}>
+            {category}
+          </span>
+        </div>
       </div>
-      <button 
-        onClick={onDelete}
-        aria-label="할 일 삭제"
-        className="w-10 h-10 flex items-center justify-center rounded-[8px] text-[20px] leading-[30px]"
-      >
-        🗑️
-      </button>
+
+      <div className={`flex gap-2 items-center justify-center flex-shrink-0 ${isEditing ? 'self-start' : 'self-center'}`}>
+        {isEditing ? (
+          <>
+            <button
+              onClick={handleSave}
+              className="flex items-center justify-center px-4 py-[10px] rounded-[8px] bg-primary text-white text-caption"
+            >
+              저장
+            </button>
+            <button
+              onClick={handleCancel}
+              className="flex items-center justify-center px-4 py-[10px] rounded-[8px] bg-white border-2 border-border text-secondary text-caption"
+            >
+              취소
+            </button>
+          </>
+
+        ) : (
+          <>
+            <button 
+              onClick={() => setIsEditing(true)}
+              aria-label="할 일 수정"
+              className="w-10 h-10 flex items-center justify-center rounded-[8px] text-[20px] leading-[30px]"
+            >
+              ✏️
+            </button>
+            <button 
+              onClick={onDelete}
+              aria-label="할 일 삭제"
+              className="w-10 h-10 flex items-center justify-center rounded-[8px] text-[20px] leading-[30px]"
+            >
+              🗑️
+            </button>
+          </>
+        )}
+      </div>
+      
     </div>
   )
 }
