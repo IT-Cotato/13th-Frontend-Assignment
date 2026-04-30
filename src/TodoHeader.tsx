3769ffd1d02d5074481b951b40e7e46e88ba3b62
YoungI422 <dlsrudtj@gmail.com>
2026-04-30 23:26:35 +0900

    refactor: TodoHeader inline 스타일을 CSS 클래스로 분리

--- src/TodoHeader.tsx
+++ src/TodoHeader.tsx
@@ -9,14 +9,7 @@ export default function TodoHeader({
 }) {
   const title = "오늘의 할 일";
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "12px",
-        alignSelf: "stretch",
-      }}
-    >
+    <div className="TodoHeader">
       <div className="Heading">
         <div className="icon">✅</div>
         <h1 className="title">{title}</h1>
