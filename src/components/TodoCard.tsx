cbb8dbbff752ea1a9d4cb1fffb11115167c8d398
hyotju <joseph20010604@gmail.com>
2026-05-03 14:14:22 +0900

    feat: 카테고리 태그 선택 및 저장 기능 구현

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,13 +3,11 @@
 interface TodoCardProps {
     task: string;
     isDone: boolean;
-    num: number;
   }
   
-  export default function TodoCard({ task, isDone, num }: TodoCardProps) {
+  export default function TodoCard({ task, isDone }: TodoCardProps) {
     return (
       <div className="todo-card">
-        {/* 1. 체크박스 영역 (기존 코드 유지) */}
         <div className={`checkbox-container ${isDone ? 'checked' : 'unchecked'}`}>
           {isDone && (
             <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -18,9 +16,8 @@ interface TodoCardProps {
           )}
         </div>
   
-        {/* 2. 🌟 텍스트 영역 (새로운 구조): 제목과 설명을 세로로 나열 */}
         <div className="task-content">
-          {/* 제목 글씨 (피그마의 '검은 글씨 + 굵게' 스타일 반영) */}
+
           <h3 className={`task-title ${isDone ? 'done-text' : ''}`}>
             {task}
           </h3>
--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
   export default function TodoHeader({ subtitle }: HeaderProps) {
     return (
       <header className="todo-header">
-        {/* 받아온 소제목을 여기에 쏙 넣습니다 */}
+
         <p className="subtitle">{subtitle}</p>
         <h1 className="title">✅ 오늘의 할 일</h1>
       </header>
