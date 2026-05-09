5d8d7370788874d0fb8dd3702e013f437093ab52
YoungI422 <dlsrudtj@gmail.com>
2026-05-09 20:00:39 +0900

    accesibility: TodoCard의 체크박스 aria-label에 할 일 추가

--- src/TodoCard.tsx
+++ src/TodoCard.tsx
@@ -30,7 +30,7 @@ export default function TodoCard({
           type="checkbox"
           checked={isCompleted}
           onChange={onToggle}
-          aria-label={isCompleted ? "할 일 완료 취소" : "할 일 완료"}
+          aria-label={`${text} ${isCompleted ? "완료 취소" : "완료"}`}
           className="TodoCard-checkbox-input"
         />
         <div className={`TodoCard-checkbox ${isCompleted ? "checked" : ""}`}>
