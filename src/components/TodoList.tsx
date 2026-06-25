43cbd14bbbe4675498616c666dba6b9db602cbb7
idid10 <ejwuud2@naver.com>
2026-06-25 18:02:18 +0900

    refactor: TodoCare 중복 key 제거

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -21,9 +21,7 @@ return (
     <ul style={listStyle}>
       {items.map((item) => (
         <li key={item.id}>
-          {/* key를 TodoCard에 지정하면 id가 바뀔 때 isEditing 등 내부 state가 리셋됨 */}
           <TodoCard
-            key={item.id}
             id={item.id}
             task={item.task}
             isCompleted={item.isCompleted}
